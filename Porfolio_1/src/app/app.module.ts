4f1bcde9b600e82cba90f1054b77471b1d4afc38
alu2023 <alu.arias16@gmail.com>
2022-08-05 18:02:48 -0300

    creacion de componentes heater and logo arg_prog

--- Porfolio_1/src/app/app.module.ts
+++ Porfolio_1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoArgProgComponent } from './componentes/logo-arg-prog/logo-arg-prog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent
   ],
   imports: [
     BrowserModule,
